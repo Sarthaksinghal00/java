d4df5bbf6b53950765017a94ea7e9ab560a039e7
Sarthaksinghal00 <sarthaksinghal0910@gmail.com>
2024-06-01 05:19:23 +0530

    newone

--- cwh_11_str.java
+++ cwh_11_str.java
@@ -1,15 +1,18 @@
 public class cwh_11_str {
     public static void main(String[] args) {
         System.out.println("jai shree ram ");//introducting str 
-        // String name;
-        // name =new String("sarthak ");
+        String name;
+        name =new String("sarthak ");
         // String name ="sarthak";
         // System.out.println(name.toUpperCase());
         // System.out.println(name.length());
         // System.out.println(name.toLowerCase());
        
-        String noTrimmed ="      sarthak        singhal";
-        System.out.println(noTrimmed.trim());
+        String noTrimmed = "      sarthak        singhal";
+        System.out.println(noTrimmed.trim());  // its remove starting spacing
+        System.out.println(name.substring(2,5));
+        System.out.println(name.replace('a','u'));
+
 
 
 
