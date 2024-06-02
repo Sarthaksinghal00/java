2575a6389213f4d5fbe5147cdcb2b9c7e4c2b963
Sarthaksinghal00 <sarthaksinghal0910@gmail.com>
2024-06-02 22:09:50 +0530

    updata in cwh_19ps

--- cwh_19.java
+++ cwh_19.java
@@ -19,52 +19,19 @@ public class cwh_19 {
 
         System.out.println( " enter the maths marks  :- " );
         int math = sc.nextInt();
-
-        if (math < 33)   {
-           
-            System.out.println(" pls next time better marks ");
-            
-        }
-    
-        else{
-            System.out.println(" good");
-        }
-          
-
         System.out.println( " enter the  science  :- " );
         int science = sc.nextInt();
-        if (science < 33)   {
-           
-            System.out.println(" pls next time better marks ");
-            
-        }
-    
-        else{
-            System.out.println(" good");
-        }
-
         System.out.println( " enter the  sst  :- " );
         int sst = sc.nextInt();
-
-        if (sst < 33)   {
-           
-            System.out.println(" pls next time better marks ");
-            
-        }
-    
-        else{
-            System.out.println(" good");
-        }
-
         float sum =  ((math + science + sst) /300f) *100f;
         // System.out.println( "your marks is "+sum +"%" );
 
-        if (sum >= 33){
+        if (sum >= 40 && math >=33 && science >=33 && sst>33 ){
             System.out.println(" you pass " + sum);
         }
          
         else{
-            System.out.println(" ohh betr luck next time ");
+            System.out.println(" ohh betr luck next time " + sum + " % ");
         }
 
 
