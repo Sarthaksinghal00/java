e91d8de7d70f79ee53c261898b458502ece18cb2
Sarthaksinghal00 <sarthaksinghal0910@gmail.com>
2024-06-14 22:23:29 +0530

    arr3y

--- cwh_26_arry.java
+++ cwh_26_arry.java
@@ -2,5 +2,11 @@ public class cwh_26_arry {
     public static void main(String[] args) {
         System.out.println("jai shree ram ");
         System.out.println("array");
+// 26 viedo code with harry 
+        int [] marks ={98,99,87,75,75,98};
+        System.out.println(marks[4]);
+        //  27 viedo code with harry 
+
+
     }
 }
