4e34f9f12c608d127bdc1ba992a7efaada7622b0
Sarthaksinghal00 <sarthaksinghal0910@gmail.com>
2024-06-17 16:26:45 +0530

    uu

--- cwh_26_arry.java
+++ cwh_26_arry.java
@@ -3,9 +3,25 @@ public class cwh_26_arry {
         System.out.println("jai shree ram ");
         System.out.println("array");
 // 26 viedo code with harry 
-        int [] marks ={98,99,87,75,75,98};
-        System.out.println(marks[4]);
-        //  27 viedo code with harry 
+        // int [] marks ={98,99,87,75,75,98};
+        // System.out.println(marks[4]);
+        //  27 viedo code with harry =========
+        // int [] marks ={98,99,87,75,75,98};
+      
+        // disply array using for loop 
+        // for (int i=0; i<marks.length;i++){
+        //     System.out.println(marks[i]);
+        // }
+
+        // display th array using for each loop
+        // for(int element:marks){
+        //     System.out.println(element);
+        // }
+        // 28 viedo code with harry multidimensinal arraya in java 1
+        
+
+
+
 
 
     }
