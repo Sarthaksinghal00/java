e4c7624860e9c704cb8cda8d01d504bbc6ff7ec2
Sarthaksinghal00 <sarthaksinghal0910@gmail.com>
2024-06-20 19:27:06 +0530

    method

---
+++ chw_31.java
@@ -0,0 +1,38 @@
+public class chw_31 {
+    
+        // create funxtion or method do not use more line 
+        static int logic(int x,int y){
+            int z;
+            if (x>y){
+                z=x+y;
+
+            }
+            else{
+                z=(x+y)*5;
+
+            }
+            return z;
+        }
+        public static void main(String[] args) {
+        System.out.println("jai shree ram ");
+        // methid overloading in java 
+        int a=6;
+        int b=9;
+        int v=logic(a,b);
+        System.out.println(v);
+
+
+
+        }
+        
+
+
+
+
+
+
+
+    
+}
+    
+
--- cwh_ps_29_ch6.java
+++ cwh_ps_29_ch6.java
@@ -13,7 +13,46 @@ public class cwh_ps_29_ch6 {
 
        // /question 2 
 
-       
+    //    int [] ss={8,89,98,989,867};
+    //    int sin=89;
+    //    boolean sarthakArry=false;
+    //    for(int element:ss){
+    //     if(sin==element){
+    //         sarthakArry=true;
+    //         break;
+    //     }
+    //    }
+    //    if(sarthakArry){
+    //     System.out.println("thr value is present in array");
+    //    }
+    //    else{
+    //     System.out.println("the value is not present in the arry ");
+    //    }
+    //question 3
+    // int [] physic ={56,76,46,98};
+    // int ram=0;
+    // for(int sarthak:physic){
+    //     ram=(ram+sarthak)/physic.length;
+    // }
+    // System.out.println("the average marks in physic in "+ram);
+
+    // question 4 
+
+   // question 5
+   int [] ss={2,4};
+   for(int element:ss){
+    System.out.println(element.);
+   }
+        
+
+
+
+
+
+
+
+
+
         
 
 
